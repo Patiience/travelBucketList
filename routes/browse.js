c6ec3e63b526a9bbe91a1fe790703b0ac0017b43
Daniel Chen <dchen117@terpmail.umd.edu>
2025-12-14 14:22:13 -0500

    implemented browse.js

--- routes/browse.js
+++ routes/browse.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const FactComment = require('../schemas/factComment');
 
-// http://localhost:3000/browse/
-router.get("/", (request, response) => {
-    response.render("browsePage");
+router.get("/", async (request, response) => {
+    await mongoose.connect(process.env.MONGO_CONNECTION_STRING);
+    FactComment.find({}).then(factComments => {
+        let browseContent = '';
+        factComments.forEach(factComment => {
+            browseContent += `<h2>${factComment.fact}</h2>`;
+            factComment.comments.forEach(comment => {
+                browseContent += `<p>- ${comment}</p>`;
+            });
+        });
+        response.render("browsePage", { browseContent: browseContent });
+        mongoose.disconnect();
+    });
 });
 
 module.exports = router;
