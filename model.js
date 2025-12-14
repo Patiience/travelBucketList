0156932cfcfe24f9378de1e6ccf61ac0fd576e20
Daniel Chen <dchen117@terpmail.umd.edu>
2025-12-13 19:01:58 -0500

    reorganized

---
+++ model.js
@@ -0,0 +1,7 @@
+const mongoose = require(mongoose);
+const factCommentSchema = new mongoose.Schema({
+    url: String,
+    comments: String
+})
+
+module.exports = mongoose.model('FactComment', factCommentsSchema);
